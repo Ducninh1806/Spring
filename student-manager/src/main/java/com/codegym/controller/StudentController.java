679b27cd6a53285fa6e4f011b2d8e14444f9eb09
ducninh1806 <ducninh1806@gmail.com>
2019-10-03 16:20:36 +0700

    Delete and Edit

--- student-manager/src/main/java/com/codegym/controller/StudentController.java
+++ student-manager/src/main/java/com/codegym/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -41,4 +42,46 @@ public class StudentController {
         return modelAndView;
     }
 
+    @GetMapping("/edit-student/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id){
+        Student student = studentService.findById(id);
+        if(student != null) {
+            ModelAndView modelAndView = new ModelAndView("/student/edit");
+            modelAndView.addObject("student", student);
+            return modelAndView;
+
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-student")
+    public ModelAndView updateStudent(@ModelAttribute("student") Student student){
+        studentService.save(student);
+        ModelAndView modelAndView = new ModelAndView("/student/edit");
+        modelAndView.addObject("student", student);
+        modelAndView.addObject("message", "Student updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-student/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id){
+        Student student = studentService.findById(id);
+        if(student != null) {
+            ModelAndView modelAndView = new ModelAndView("/student/delete");
+            modelAndView.addObject("student", student);
+            return modelAndView;
+
+        }else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-student")
+    public String deleteStudent(@ModelAttribute("student") Student student){
+        studentService.remove(student.getId());
+        return "redirect:student";
+    }
 }
